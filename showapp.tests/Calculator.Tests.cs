e1e8072a22e183b1feee08f293325764af603f17
Anton Lunev <lunev.a.anton@gmail.com>
2023-02-19 20:58:39 +0000

    Showapp test added

---
+++ showapp.tests/Calculator.Tests.cs
@@ -0,0 +1,62 @@
+using Xunit;
+using showapp.services;
+
+namespace showapp.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CalculatorAddTestsInts()
+    {
+        //  
+        int a = 1, b = 2;
+        var calc = new Calculator();
+        
+        var x = calc.Add(a,b);
+        
+        // assert
+        Assert.IsType<int>(x);
+    }
+
+    [Fact]
+    public void CalculatorAddTestsFloats()
+    {
+        //  
+        double a = 1.1, b = 2.2;
+        var calc = new Calculator();
+        
+        var x = calc.Add(a,b);
+        
+        // assert
+        Assert.IsType<double>(x);
+    }
+
+    [Fact]
+    public void CalculatorAddTestsFloats2()
+    {
+        //  
+        double a = 1.1;
+        int b = 2;
+        var calc = new Calculator();
+        
+        var x = calc.Add(a,b);
+        
+        // assert
+        Assert.IsType<double>(x);
+    }
+
+    [Fact]
+    public void CalculatorAddTestsFloats3()
+    {
+        //  
+        double b = 1.1;
+        int a = 2;
+        var calc = new Calculator();
+        
+        var x = calc.Add(a,b);
+        
+        // assert
+        Assert.IsType<double>(x);
+    }
+
+}   
